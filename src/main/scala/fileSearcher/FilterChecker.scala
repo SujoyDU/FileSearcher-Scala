62eb2a06f003cacfb7eec6421d9bb742172686ea
sujoyDohatec <sujoy@dohatec.com.bd>
2017-02-19 15:55:29 +0600

    file content check added

--- src/main/scala/fileSearcher/FilterChecker.scala
+++ src/main/scala/fileSearcher/FilterChecker.scala
@@ -1,19 +1,37 @@
 package fileSearcher
 
+import java.io.File
+
+import scala.util.control.NonFatal
+
 /**
   * Created by sujoy on 2/14/2017.
   */
-class FilterChecker (filter : String) {
+class FilterChecker(filter: String) {
 
-  def matches(content : String) = content contains filter
+  def matches(content: String) = content contains filter
 
-  def findMatchedFiles( iOObjects : List[IOObject] ) =
-    for ( iOObject <- iOObjects
-      if(iOObject.isInstanceOf[FileObject])
-          if(matches(iOObject.name)))
+  def findMatchedFiles(iOObjects: List[IOObject]) =
+    for (iOObject <- iOObjects
+         if (iOObject.isInstanceOf[FileObject])
+         if (matches(iOObject.name)))
       yield iOObject
 
+  def matchesFileContent(file: File) = {
+    import scala.io.Source
+    try {
+      val fileSource = Source.fromFile(file)
+      try
+        fileSource.getLines() exists(line => matches(line))
+      catch {
+        case NonFatal(_) => false
 
+      }
+    }
+    catch {
+      case NonFatal(_) => false
+    }
+  }
 }
 
 object FilterChecker {
--- src/test/scala/fileSearcher/FilterCheckerTests.scala
+++ src/test/scala/fileSearcher/FilterCheckerTests.scala
@@ -22,4 +22,16 @@ class FilterCheckerTests extends FlatSpec {
       val matchedFiles = FilterChecker("match").findMatchedFiles(listOfIOObjects)
       assert( matchedFiles.length == 0)
   }
+
+  "FilterChecker passed a file with content that matches the filter" should
+    "return that the match succeeded" in {
+      val isContentMatched = FilterChecker("pluralsight").matchesFileContent(new File("./testfiles/pluralsight.data"))
+      assert( isContentMatched == true)
+  }
+
+  "FilterChecker passed a file with content that does not match the filter" should
+    "return that the match failed" in {
+      val isContentMatched = FilterChecker("pluralsight").matchesFileContent(new File("./testfiles/readme.txt"))
+      assert( isContentMatched == false )
+  }
 }
